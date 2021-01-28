39d8b4abdfff3bfb6bd6164ee4b7a9c7455babb0
Anush Jain <ajain@argusoft.com>
2021-01-28 15:19:11 +0530

    global controller return added

--- nodejs-server/helper/wrapper.js
+++ nodejs-server/helper/wrapper.js
@@ -4,9 +4,9 @@ errorHandler = (fn) => async (req, res, next) => {
   try {
     await fn(req, res, next)
   } catch (error) {
-    console.log(error)
     if (error instanceof AppError) {
       res.status(error.statusCode).send({ msg: error.message })
+      return;
     }
     res.status(500).send({ msg: 'Internal Server Error' })
   }
